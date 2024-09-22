cbe4b6661dcc820d1ea1837e35c207bc4832e21a
ZuriIsHere <mejidovazuri1400@gmail.com>
2024-09-22 21:50:19 +0300

    spring bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean == bean2);
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat == cat2);
     }
 }
